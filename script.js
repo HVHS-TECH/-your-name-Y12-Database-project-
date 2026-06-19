dfec7cb856af81c3676bb6a9c738685ea45a9c26
ytyfuigoljfh <23367bs@hvhs.school.nz>
2026-06-19 12:29:12 +1200

    a

--- script.js
+++ script.js
@@ -1,8 +1,50 @@
 
+function fb_login(){
+  authenticationListener = firebase.auth().onAuthStateChanged(fb_handleLogin);
+};
 
-function lancer(){
-  console.log("Running lancer()")
-  firebase.database().ref('/').set(
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLpZSBThXbbwMCqZaa1d8s9GUMl4QRNaTHIQ&s"
-  )
+function fb_handleLogin(_user){
+  if(_user){
+    console.log("user is logedined")
+    GLOBAL_user = _user 
+  } else {
+    fb_popupLogin();
+    console.log("loging in user")
+  };
+};
+
+function fb_popupLogin(){
+  var provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase.auth().signInWithPopup(provider).then((result) => {
+    GLOBAL_user = result.user;
+    console.log("log in user")
+  })
+};
+
+function fb_logout(){
+  authenticationListener();
+  firebase.auth().signOut();
+  console.log("loged out >:) evilness")
+};
+
+function writeForm(){
+    // Get the form data
+    const age = document.getElementById("age").value;
+    const name = document.getElementById("name").value;
+    console.log(favoriteCash)
+    console.log(cashQuantity)
+    console.log(favImg)
+    console.log(name)
+    firebase.database().ref('/users').set(GLOBAL_user.uid)
+    firebase.database().ref('/users' + GLOBAL_user.uid).set({
+      name: name,
+      favoriteCash: favoriteCash,
+      cashQuantity: cashQuantity,
+      favImg: favImg,
+      favImgalt: favImgalt
+    })
+    firebase.database().ref('/users').once("value", display, fb_readError)
+    firebase.database().ref('/users' + GLOBAL_user.uid).once("value", display, fb_readError)
+    firebase.database().ref('/').once("value", displayDatabase, fb_readError)
 };
